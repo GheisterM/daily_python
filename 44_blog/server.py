498ae995899207ad4fcd8354d0715d221d9b8040
Gheister Morillo <gheisterm@gmail.com>
2024-12-03 21:00:04 -0400

    (Update) Day 42: Upgraded contact form and route to work with smtplib.

--- 44_blog/server.py
+++ 44_blog/server.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+import smtplib
+from dotenv import load_dotenv
+from os import environ
+from flask import Flask, render_template, request
 from post import Post
 
+load_dotenv()
 app = Flask(__name__)
 posts = Post()
+
+EMAIL_USER = environ.get('MY_EMAIL')
+EMAIL_PASS = environ.get('MY_EMAIL_PASSWORD')
 
 
 @app.route('/')
@@ -21,9 +28,29 @@ def about_fake(index):
     return render_template('about.html', post=current_post)
 
 
-@app.route('/contact-us')
+@app.route('/contact-us', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'GET':
+        return render_template('contact.html', message='Contact')
+    else:
+        subject = "Subject:New Message!\n\n"
+        message_body = f"""
+            Name: {request.form['name']}
+            Email: {request.form['email']}
+            Phone: {request.form['phone']}
+            Message: {request.form['message']}
+        """
+        final_msg = subject + message_body
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=EMAIL_USER, password=EMAIL_PASS)
+            connection.sendmail(
+                from_addr=EMAIL_USER,
+                to_addrs=EMAIL_USER,
+                msg=final_msg
+            )
+        return render_template('contact.html', message='Successfully sent message')
 
 
 @app.route('/posts/<int:index>')
